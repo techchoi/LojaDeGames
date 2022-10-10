9e4ae7ea60ee8524aacc4c77b90d928a9ba27fcc
techchoi <ninahjc.94@gmail.com>
2022-10-10 09:19:57 -0300

    arrumando erro

--- src/main/java/com/generation/lojaDeGames/repository/CategoriasRepository.java
+++ src/main/java/com/generation/lojaDeGames/repository/CategoriasRepository.java
@@ -3,9 +3,11 @@ package com.generation.lojaDeGames.repository;
 import com.generation.lojaDeGames.model.CategoriasModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CategoriasRepository extends JpaRepository<CategoriasModel, Long> {
     public List<CategoriasModel> findAllByDescricaoContainingIgnoreCase(@Param("descricao") String descricao);
 }
