7833a471ee8996d780a5a5d786af91c2ea1b8726
techchoi <ninahjc.94@gmail.com>
2022-10-10 09:20:47 -0300

    arrumando erros

--- src/main/java/com/generation/lojaDeGames/repository/ProdutosRepository.java
+++ src/main/java/com/generation/lojaDeGames/repository/ProdutosRepository.java
@@ -4,11 +4,9 @@ package com.generation.lojaDeGames.repository;
 import com.generation.lojaDeGames.model.ProdutosModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface ProdutosRepository extends JpaRepository<ProdutosModel, Long> {
     public List<ProdutosModel> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
 }
